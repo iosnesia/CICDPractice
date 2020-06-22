8da9bc6a7e54052155b91be70c178ac589295312
iosnesia <helloiosnesia@gmail.com>
2020-06-22 16:07:34 +0700

    initial commit

---
+++ CICDPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CICDPractice
+//
+//  Created by Firda Sahidi on 20/06/20.
+//  Copyright © 2020 Firda Sahidi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
