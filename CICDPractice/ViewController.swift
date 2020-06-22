133e863a06ebb8599b21634221abc54717313990
iosnesia <helloiosnesia@gmail.com>
2020-06-22 16:59:59 +0700

    Add Unit Test

--- CICDPractice/ViewController.swift
+++ CICDPractice/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstNumberTextField: UITextField!
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func CountButtonTapped(_ sender: Any) {
+        let num1 = Int(firstNumberTextField.text ?? "0")
+        let num2 = Int(secondNumberTextField.text ?? "0")
+        answerLabel.text = "\(count(num1: num1 ?? 0, num2: num2 ?? 0))"
+    }
+    
+    func count(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
 }
 
--- CICDPracticeTests/CICDPracticeTests.swift
+++ CICDPracticeTests/CICDPracticeTests.swift
@@ -10,25 +10,27 @@ import XCTest
 @testable import CICDPractice
 
 class CICDPracticeTests: XCTestCase {
-
+    var vc1: ViewController!
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        let storyboard = UIStoryboard(name: "Main",
+                                      bundle: nil)
+        let viewController1 =
+            storyboard.instantiateViewController(
+                withIdentifier: "vc1")
+        vc1 = viewController1
+            as? ViewController
+        vc1.loadViewIfNeeded()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testCalculationIsRight() {
+        let result = vc1.count(num1: 2, num2: 6)
+        XCTAssertEqual(result, 8)
     }
 
 }
