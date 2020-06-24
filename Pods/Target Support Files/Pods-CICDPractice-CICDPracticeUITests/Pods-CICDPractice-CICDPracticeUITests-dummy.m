8ab3b2618449e4126cab7d67fbf7cd50d85d5ebd
iosnesia <helloiosnesia@gmail.com>
2020-06-24 22:19:47 +0700

    Add Pod

---
+++ Pods/Target Support Files/Pods-CICDPractice-CICDPracticeUITests/Pods-CICDPractice-CICDPracticeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CICDPractice_CICDPracticeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CICDPractice_CICDPracticeUITests
+@end
---
+++ Pods/Target Support Files/Pods-CICDPracticeTests/Pods-CICDPracticeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CICDPracticeTests : NSObject
+@end
+@implementation PodsDummy_Pods_CICDPracticeTests
+@end
